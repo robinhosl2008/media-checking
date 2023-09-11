7e0ec0acc318ea508916db12bd97314ed6e168fe
Robson Souza <robson.souza@onbusdigital.com.br>
2023-09-11 12:36:00 -0300

    Adicionando os componentes alerts e loading

---
+++ resources/views/components/loading.blade.php
@@ -0,0 +1,4 @@
+<div class="loadImage" style="display: none">
+    <img style="width: 50px; height: 50px; position: fixed; left: 50%; top: 50%; z-index: 9999;" 
+        src="{{ asset('img/loading-25.gif') }}">
+</div>
