a8aa7cfa375861951eed15fde248c5168292e185
Robson Souza <robson.souza@onbusdigital.com.br>
2023-09-11 12:34:04 -0300

    Adicionando o js que trata dos alertas.

--- public/js/midia-checking/validar_midia.js
+++ public/js/midia-checking/validar_midia.js
@@ -9,4 +9,27 @@ window.onload = function() {
             file.readAsDataURL(this.files[0]);
         }
     }, false);
+
+    // var btnLaranjaValidar = document.querySelector('.btn-laranja-validar');
+    // btnLaranjaValidar.addEventListener('click', function() {
+
+        
+
+        // var modal = $('#exampleModal');
+        // modal.show();
+    // })
+}
+
+function validarFormulario() {
+    var tipoMidia = document.getElementById('tipo_midia').value,
+        vertical = document.getElementById('vertical').value,
+        produto = document.getElementById('produto').value,
+        arquivo = document.getElementById('arquivo');
+
+    if (tipoMidia == 0 || vertical == 0 || produto == 0 || (!arquivo.files && !arquivo.files[0])) {
+        exibirAlertaDeInput('warning', 'Todos os campos são obrigatórios.');
+        return;
+    }
+
+    document.querySelector('#open-modal').click();
 }
